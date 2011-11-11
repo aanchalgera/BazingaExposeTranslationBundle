d534e270b978da52e93094a1a1404b6717bcdc74
kashmir singh thakur <royalrajputs.88@gmail.com>
2011-11-11 13:56:49 +0530

    added functionality to write the console output to a fileand some minor changes to input arguments

--- Command/extractmessagesCommand.php
+++ Command/extractmessagesCommand.php
@@ -5,42 +5,47 @@ namespace Bazinga\ExposeTranslationBundle\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Bazinga\ExposeTranslationBundle\Controller\Controller;
-use Symfony\Component\Templating\PhpEngine;
-use Symfony\Component\Templating\TemplateNameParser;
 
-use Symfony\Component\HttpFoundation\Response;
-use Bazinga\ExposeTranslationBundle\Service\TranslationFinder;
-
-class extractmessagesCommand extends ContainerAwareCommand
+/**
+ * Dumps translations to the filesystem.
+ *
+ * @author Kashmir Singh Thakur <kashmir@agilemedialab.in>
+ */
+class DumpTranslationsCommand extends ContainerAwareCommand
 {
-    
     protected function configure()
     {
         $this
-            ->setName('bazinga:messages')
-            ->setDescription('extracts messages from a given domain for a user locale')
-            ->setDefinition(array());
-        //        new InputArgument('domain_name', InputArgument::REQUIRED, 'Domain Name'),
-        //        new InputArgument('locale', InputArgument::REQUIRED, 'Locale'),
-        //        new InputArgument('format', InputArgument::REQUIRED, 'Format'),
-        //        outputfile
-        //        ))
-        //;
+            ->setName('bazinga:translations:dump')
+            ->setDescription('Extracts translations from a given domain for a user locale and outputs them in javascript')
+            ->addArgument('domain_name', InputArgument::REQUIRED, 'Translation message domain')
+            ->addArgument('locale', InputArgument::REQUIRED, 'Locale')
+            ->addArgument('file_location', InputArgument::REQUIRED, 'Output file location');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $domain_name = 'messages'; //$input->getArgument('domain_name');
-        $locale      = 'es'; //$input->getArgument('locale');
-        $format      = 'js'; //$input->getArgument('format');
+        $domain_name = $input->getArgument('domain_name');
+        $locale      = $input->getArgument('locale');
+        $path        = $input->getArgument('file_location');
+        $format      = 'js'; 
         
         $controller = $this->getContainer()->get('bazinga.exposetranslation.controller');
         $result = $controller->exposeTranslationAction($domain_name, $locale, $format);
-
-        // output to file
-        print_r($result->getContent());
+        
+        $this->write($path, $result->getContent());
     }
+    
+    private function write($path, $contents)
+    {
+        if (!is_dir($dir = dirname($path)) && false === @mkdir($dir, 0777, true)) {
+            throw new \RuntimeException('Unable to create directory '.$dir);
+        }
+
+        if (false === @file_put_contents($path, $contents)) {
+            throw new \RuntimeException('Unable to write file '.$path);
+        }
+    }
+
 }
